18d5970b243abc9da5a6c6e8e8531ad849c27710
Claziero <lorenzoclazzer.2000@outlook.it>
2022-03-11 21:01:20 +0100

    Aggiunta commenti, revisione del codice, eliminazione parentesi

--- frogger_astar.py
+++ frogger_astar.py
@@ -18,40 +18,43 @@ WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("First Game!")
 FPS = 30
 
-#---Elementi utili---
-#Coda di priorità contenente i nodi scoperti non visitati
+#---Elementi utili---#
+# Coda di priorità contenente i nodi scoperti non visitati
 esplorare = PriorityQueue()
-#Coda di priorità contenente i nodi scoperti già visitati
+# Coda di priorità contenente i nodi scoperti già visitati
 visitati = PriorityQueue()
-#Costante costo
+# Costante costo
 COSTO = 1
 
-#Classe Nodo
+# Classe Nodo
 class Nodo():
-    #Costruttore
+    # Costruttore
     def __init__(self, pos = None, precedente = None, tempo = 0) -> None:
-        self.g = 0
-        self.h = 0
-        self.f = 0
-        self.posizione = pos
-        self.precedente = precedente
-        self.tempo = tempo
+        self.g = 0                      # Costo del percorso finora sostenuto
+        self.h = 0                      # Costo stimato per raggiungere il goal
+        self.f = 0                      # Costo totale del percorso
+        self.posizione = pos            # Posizione nel formato (x, y)
+        self.precedente = precedente    # Nodo padre per risalire al percorso
+        self.tempo = tempo              # Tempo di generazione del nodo (0..15)
 
-    #Ridefinizione del concetto di uguaglianza tra nodi: devono essere nella stessa posizione allo stesso tempo
+    # Ridefinizione del concetto di uguaglianza tra nodi: 
+    # devono essere nella stessa posizione allo stesso tempo
     def __eq__(self, altro) -> bool:
         assert(isinstance(altro, Nodo))
-        return self.posizione == altro.posizione and self.tempo == altro.tempo #TBC
+        return self.posizione == altro.posizione and self.tempo == altro.tempo
 
-    #Ridefinizione del concetto di minore tra nodi: deve avere una f minore
+    # Ridefinizione del concetto di minore tra nodi: 
+    # il più piccolo deve avere una f minore
     def __lt__(self, altro):
         assert(isinstance(altro, Nodo))
         return self.f < altro.f
 
-    #Funzione per definire uno stato obiettivo
+    # Funzione per definire uno stato obiettivo
     def obiettivo(self) -> bool:
         return self.posizione[0] == 0
 
 class frogger_game:
+    # Costruttore
     def __init__(self):
         self.state_matrix = np.asarray([
             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -90,6 +93,7 @@ class frogger_game:
 
         return lose, win
 
+    # Agente a riflessi semplice
     def simple_reactive(self):
         action = 0
         if (self.frog_pos[0] >= 4 and self.frog_pos[0] <= 5):
@@ -101,7 +105,8 @@ class frogger_game:
 
         return action
 
-    #Funzione per aggiornare il campo scorrendo nelle due direzioni
+    # Funzione per aggiornare il campo scorrendo nelle due direzioni
+    # tramite il parametro 'incremento' (positivo = scorrimento normale)
     def aggiorna_campo(self, incremento) -> None:
         r1 = collections.deque(self.state_matrix[1])
         r2 = collections.deque(self.state_matrix[2])
@@ -123,53 +128,58 @@ class frogger_game:
         r6 = list(r6)
         self.state_matrix = np.asarray([self.state_matrix[0], r1, r2, r3, r4, r5, r6, self.state_matrix[7]])
         
-    #Funzione per controllare una mossa: restituisce True se valida, False se porta alla perdita
+    # Funzione per controllare una mossa: 
+    # restituisce True se valida, False se porta alla perdita
     def controlla_pos(self, posizione) -> bool:
-        if (self.state_matrix[posizione[0]][posizione[1]] != 0):
-            return False    #Perso
-        return True    #Non perso
+        if self.state_matrix[posizione[0]][posizione[1]] != 0:
+            return False    # Perso
+        return True         # Mossa valida
 
-    #Funzione per provare le mosse possibili in ogni punto
+    # Funzione per provare le mosse possibili in ogni punto
     def get_neighbors(self, nodo_corrente:Nodo):
         adjac_lis = []
 
-        #Incrementa il tempo e fai scorrere il campo (tempo modulo larghezza della mappa)
+        # Incrementa il tempo e fai scorrere il campo 
+        # (tempo modulo larghezza della mappa)
         tempo = (nodo_corrente.tempo + 1) % 16
         self.aggiorna_campo(tempo)
 
-        #Per ogni azione disponibile
+        # Per ogni azione disponibile
         for action in range(5):
-            if(action == 0):    #NONE
+            if action == 0:    # NONE
                 pos = nodo_corrente.posizione
-            elif(action == 1):  #LEFT
+            elif action == 1:  # LEFT
                 pos = (nodo_corrente.posizione[0], max(0, nodo_corrente.posizione[1] - 2))
-            elif(action == 2):  #UP
+            elif action == 2:  # UP
                 pos = (max(0, nodo_corrente.posizione[0] - 1), nodo_corrente.posizione[1])
-            elif(action == 3):  #RIGHT
+            elif action == 3:  # RIGHT
                 pos = (nodo_corrente.posizione[0], min(15, nodo_corrente.posizione[1] + 2))
-            elif(action == 4):  #DOWN
+            elif action == 4:  # DOWN
                 pos = (min(7, nodo_corrente.posizione[0] + 1), nodo_corrente.posizione[1])
 
-            #Prova la mossa scelta
+            # Prova la mossa scelta
             mossa_buona = self.controlla_pos(pos)
             
-            #Se l'azione non porta alla perdita, aggiungi un nuovo nodo alla lista dei vicini
-            if(mossa_buona):
-                #Imposta un nuovo nodo con la sua posizione e aggiungilo alla lista adiacenti
+            # Se l'azione non porta alla perdita, aggiungi un nuovo nodo alla lista dei vicini
+            if mossa_buona:
+                # Imposta un nuovo nodo con la sua posizione e aggiungilo alla lista adiacenti
                 n = Nodo(pos, nodo_corrente, tempo)
                 adjac_lis.append(n)
 
-        #Ripristina lo stato precedente del gioco
+        # Ripristina lo stato precedente del gioco
         self.aggiorna_campo(-tempo)
         return adjac_lis
 
-    def h(self, n:Nodo): #H
+    # Euristica: righe mancanti all'obiettivo - 1
+    def h(self, n:Nodo): # H
         distanceFromEndLane = n.posizione[0] - 1
         return distanceFromEndLane
 
+    # Funzione inutilizzata
     def mapAState(self, position, matrix):
         self.statemap[position] = matrix
 
+    # Funzione che restituisce un'azione date 2 posizioni consecutive
     def a_star_path_to_actions(self):
         if not self.path:
             self.path = self.A_star_agent()
@@ -187,83 +197,84 @@ class frogger_game:
                 return 3                # 3-right
             elif difference[1] < 0:
                 return 1                # 1-left
-        return 0               # 0-Nothing
+        return 0                        # 0-Nothing
 
+    # Agente A*
     def A_star_agent(self):
-        #Aggiungi il nodo iniziale alla coda esplorare, con costo 0
+        # Aggiungi il nodo iniziale alla coda esplorare, con costo 0
         nodo_iniziale = Nodo((7, 7))
         esplorare.put((0, nodo_iniziale))
 
         #---Inizia l'esplorazione---#
-        #Finché la lista di nodi da esplorare non è vuota 
-        while(len(esplorare.queue) != 0):
-            #Estrai il primo elemento (a costo minimo)
+        # Finché la lista di nodi da esplorare non è vuota 
+        while len(esplorare.queue) != 0:
+            # Estrai il primo elemento (a costo f minimo)
             nodo_corrente = esplorare.get()[1]
-            #E inseriscilo nella lista di nodi visitati
+            # E inseriscilo nella lista di nodi visitati
             visitati.put(nodo_corrente) 
 
-            #Se il nodo corrente corrisponde all'obiettivo allora riporta il percorso fatto
-            if(nodo_corrente.obiettivo()):
+            # Se il nodo corrente corrisponde all'obiettivo allora riporta il percorso fatto
+            if nodo_corrente.obiettivo():
                 print("Soluzione trovata")
-                #Ricostruisci il percorso dalla fine verso l'inizio
+                # Ricostruisci il percorso dalla fine verso l'inizio
                 percorso = []
-                while(nodo_corrente is not None):
+                while nodo_corrente is not None:
                     percorso.append(nodo_corrente.posizione)
                     nodo_corrente = nodo_corrente.precedente
 
-                #Restituisci il percorso inverso (inizio -> fine)
+                # Restituisci il percorso inverso (inizio -> fine)
                 print(percorso[::-1])
                 return percorso[::-1]
 
-            #Altrimenti cerca i nodi adiacenti al nodo corrente
+            # Altrimenti cerca i nodi adiacenti al nodo corrente
             adiacenti = self.get_neighbors(nodo_corrente)
 
-            #Per ogni nodo adiacente trovato
+            # Per ogni nodo adiacente trovato
             for n in adiacenti:
-                #Se il nodo è già stato visitato non fare nulla
-                if(cerca_nodo(visitati, n)):
+                # Se il nodo è già stato visitato non fare nulla
+                if cerca_nodo(visitati, n):
                     continue
 
-                #Imposta i campi del nodo (F, G, H)
+                # Imposta i campi del nodo (F, G, H)
                 n.g = nodo_corrente.g + COSTO
                 n.h = self.h(n)
                 n.f = n.g + n.h
 
-                #Controlla che il nodo da aggiungere (n) non sia già in esplorare
+                # Controlla che il nodo da aggiungere (n) non sia già in esplorare
                 presente = cerca_nodo(esplorare, n)
-                if(presente is not False):
-                    #Se c'è controlla che non abbia valore di G maggiore di quello già presente
-                    if(n.g >= presente.g):
-                        #Scarta il nodo
+                if presente is not False:
+                    # Se c'è controlla che non abbia valore di G maggiore o uguale di quello già presente
+                    if n.g >= presente.g:
+                        # Scarta il nodo
                         continue
 
-                #Aggiungi il nodo alla lista da esplorare
+                # Altrimenti aggiungi il nodo alla lista da esplorare
                 esplorare.put((n.f, n))
 
                 #---Torna ad esplorare i nodi adiacenti al corrente---#
             #---Esplora nuovi nodi, se ci sono---#
 
-        #Se sei in questo punto, non hai trovato la soluzione
+        # Se sei in questo punto, non hai trovato la soluzione
         print("Soluzione non trovata. Terminazione del programma")
         exit(1)
         return None
 
-#Funzione per cercare un nodo all'interno di una lista
+# Funzione ausiliaria per cercare un nodo all'interno di una lista
 def cerca_nodo(lista:PriorityQueue, nodo:Nodo):
-    #Se la lista è vuota ritorna subito
-    if(len(lista.queue) == 0):
+    # Se la lista è vuota ritorna subito
+    if len(lista.queue) == 0:
         return False
 
-    #Scorri tutti gli elementi
+    # Scorri tutti gli elementi
     for n in lista.queue:
-        #Se non è un nodo allora è una tupla contenente il nodo
-        if(not isinstance(n, Nodo)):
+        # Se non è un nodo allora è una tupla contenente il nodo
+        if not isinstance(n, Nodo): 
             n = n[1]
-        #Se trovi il nodo riportalo
-        if(nodo == n):
+        # Se trovi il nodo riportalo
+        if nodo == n: 
             return n
     
-    #Il nodo non è stato trovato
+    # Il nodo non è stato trovato
     return False
 
 def draw_window(game):
@@ -298,6 +309,7 @@ def main():
     lost = False
     win = False
 
+    # Generazione del campo randomica
     t = int(random() * 14 + 1)
     print("Random:", t)
     game.aggiorna_campo(t)
