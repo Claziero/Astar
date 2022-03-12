550bf898bd27a3c03da66941600d3f42b28b7307
Claziero <lorenzoclazzer.2000@outlook.it>
2022-03-12 16:47:45 +0100

    Modifiche per predisporre la stampa degli stati intermedi delle liste e dei nodi Aggiunta funzione per stampare un nodo

--- Astar.py
+++ Astar.py
@@ -1,4 +1,5 @@
 from queue import PriorityQueue
+import string
 
 #---Elementi utili---#
 # Coda di priorità contenente i nodi scoperti non visitati
@@ -9,25 +10,23 @@ visitati = PriorityQueue()
 # Classe Nodo
 class Nodo():
     # Costruttore
-    def __init__(self, pos=None, precedente=None, tempo=0) -> None:
+    def __init__(self, precedente=None) -> None:
         self.g:int = 0                          # Costo del percorso finora sostenuto
         self.h:int = 0                          # Costo stimato per raggiungere il goal
         self.f:int = 0                          # Costo totale del percorso
-        self.posizione:list[int] = pos          # Posizione nel formato (x, y)
         self.precedente:Nodo = precedente       # Nodo padre per risalire al percorso
-        self.tempo:int = tempo                  # Tempo di generazione del nodo (0..15)
 
-    # Ridefinizione del concetto di uguaglianza tra nodi: 
-    # devono essere nella stessa posizione allo stesso tempo
+    # Ridefinizione del concetto di uguaglianza tra nodi
     def __eq__(self, altro) -> bool:
         assert(isinstance(altro, Nodo))
-        return self.posizione == altro.posizione and self.tempo == altro.tempo
+        # Inserisci qui
+        return None
 
-    # Ridefinizione del concetto di minore tra nodi: 
-    # il più piccolo deve avere una f minore
+    # Ridefinizione del concetto di minore tra nodi
     def __lt__(self, altro) -> bool:
         assert(isinstance(altro, Nodo))
-        return self.f < altro.f
+        # Inserisci qui
+        return None
 
     # Funzione per definire uno stato obiettivo
     def obiettivo(self) -> bool:
@@ -45,12 +44,17 @@ class Nodo():
         # Inserire qui
         return None
 
+    # Funzione per stampare gli elementi di un nodo
+    def formato_stampa(self) -> string:
+        # Inserire qui
+        return
+
 # Classe Azione
 class Azione():
     # Costruttore
-    def __init__(self, c=0, id=None) -> None:
-        self.costo = c  # Costo dell'azione 
-        self.id = id    # Azione identificata da un numero, stringa, ...
+    def __init__(self, costo=1, id=None) -> None:
+        self.costo = costo  # Costo dell'azione 
+        self.id = id        # Azione identificata da un numero, stringa, ...
 
     # Funzione per compiere un'azione
     # Riporta il nuovo nodo se l'azione è compiuta, None se l'azione è invalida
@@ -63,8 +67,7 @@ class Azione():
 # Classe gioco
 class Gioco():
     # Costruttore
-    def __init__(self, m:list[list], ni:Nodo, ap:list[Azione]) -> None:
-        self.matrice:list[list] = m
+    def __init__(self, ni:Nodo, ap:list[Azione]) -> None:
         self.nodoIniziale:Nodo = ni
         self.azioniPossibili:list[Azione] = ap
 
@@ -87,7 +90,7 @@ class Gioco():
     # Implementazione di A*
     # Restituisce una lista di posizioni nella mappa dalla partenza all'obiettivo
     # oppure None se non c'è soluzione al problema
-    def astar_algo(self) -> list[list[int]]:
+    def astar_algo(self, stampa=False) -> list[list]:
         # Aggiungi il nodo iniziale alla coda esplorare, con costo 0
         esplorare.put((0, self.nodoIniziale))
 
@@ -100,20 +103,21 @@ class Gioco():
             visitati.put(nodoCorrente) 
 
             # Se il nodo corrente corrisponde all'obiettivo allora riporta il percorso fatto
-            if NodoCorrente.obiettivo():
-                print("Soluzione trovata")
+            if nodoCorrente.obiettivo():
+                print(">>Soluzione trovata")
                 # Ricostruisci il percorso dalla fine verso l'inizio
                 percorso:list[list[int]] = []
-                while NodoCorrente is not None:
-                    percorso.append(NodoCorrente.posizione)
-                    NodoCorrente = NodoCorrente.precedente
+                while nodoCorrente is not None:
+                    percorso.append(nodoCorrente.formato_stampa())
+                    nodoCorrente = nodoCorrente.precedente
 
+                # Stampa il percorso (se richiesto)
+                if stampa: print(">>Sol:", percorso[::-1])
                 # Restituisci il percorso inverso (inizio -> fine)
-                print(percorso[::-1])
                 return percorso[::-1]
 
             # Altrimenti cerca i nodi adiacenti al nodo corrente
-            adiacenti = self.cerca_adiacenti(NodoCorrente)
+            adiacenti = self.cerca_adiacenti(nodoCorrente)
 
             # Per ogni nodo adiacente trovato
             for n in adiacenti:
@@ -135,8 +139,22 @@ class Gioco():
                 #---Torna ad esplorare i nodi adiacenti al corrente---#
             #---Esplora nuovi nodi, se ci sono---#
 
+            # Stampa riepilogo delle liste (se richiesto)
+            if stampa:
+                print(">Lista Esplorare: ", end="||")
+                for e in esplorare.queue:
+                    e = e[1]
+                    print(e.formato_stampa(), end="||")
+                print()
+
+                print(">Lista Visitati: ", end="||")
+                for e in visitati.queue:
+                    print(e.formato_stampa(), end="||")
+                print()
+                print()
+
         # Se sei in questo punto, non hai trovato la soluzione
-        print("Soluzione non trovata.")
+        print(">>Soluzione non trovata.")
         return None
 
 # Funzione ausiliaria per cercare un nodo all'interno di una lista
